9d590936cbe58de3745cff3d6104be71fdcfaf88
Minh Hoang TCU <minh.hoang@tcu.edu>
2016-10-08 13:45:41 -0400

    fixed server

--- app.js
+++ app.js
@@ -255,10 +255,65 @@ function receivedMessage(event) {
     // keywords and send back the corresponding example. Otherwise, just echo
     // the text we received.
     switch (messageText) {
-      case 'Hi Tripper':
-      	sendTextMessage(senderID, 'Hello. How can I help you?')
-      case 'Get me a friend from Dallas to MHacks')
-		sendTextMessage(senderID, 'Finding a friend with the same route')
+      case 'image':
+        sendImageMessage(senderID);
+        break;
+
+      case 'gif':
+        sendGifMessage(senderID);
+        break;
+
+      case 'audio':
+        sendAudioMessage(senderID);
+        break;
+
+      case 'video':
+        sendVideoMessage(senderID);
+        break;
+
+      case 'file':
+        sendFileMessage(senderID);
+        break;
+
+      case 'button':
+        sendButtonMessage(senderID);
+        break;
+
+      case 'generic':
+        sendGenericMessage(senderID);
+        break;
+
+      case 'receipt':
+        sendReceiptMessage(senderID);
+        break;
+
+      case 'quick reply':
+        sendQuickReply(senderID);
+        break;        
+
+      case 'read receipt':
+        sendReadReceipt(senderID);
+        break;        
+
+      case 'typing on':
+        sendTypingOn(senderID);
+        break;        
+
+      case 'typing off':
+        sendTypingOff(senderID);
+        break;        
+
+      case 'account linking':
+        sendAccountLinking(senderID);
+        break;
+
+      case 'hello':
+      	sendTextMessage(senderID, 'Hello')
+      	break;
+
+      case 'Get me a friend from Dallas to MHacks':
+      	sendTextMessage(senderID, 'Getting you a friend!')
+      	break;
 
       default:
         sendTextMessage(senderID, messageText);
@@ -788,4 +843,3 @@ app.listen(app.get('port'), function() {
 });
 
 module.exports = app;
-
